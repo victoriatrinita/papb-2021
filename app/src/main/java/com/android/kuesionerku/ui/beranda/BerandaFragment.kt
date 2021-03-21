7e85f2328dd7a424a9d141f4f5105f21d7b8990c
victoriatrinita <victoriatrinita@yahoo.com>
2021-03-21 20:04:25 +0700

    add logs to fragments' lifecycle

--- app/src/main/java/com/android/kuesionerku/ui/beranda/BerandaFragment.kt
+++ app/src/main/java/com/android/kuesionerku/ui/beranda/BerandaFragment.kt
@@ -1,14 +1,15 @@
 package com.android.kuesionerku.ui.beranda
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.android.kuesionerku.R
+import android.util.Log
+
 
 class BerandaFragment : Fragment() {
 
@@ -28,4 +29,50 @@ class BerandaFragment : Fragment() {
         })*/
         return root
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("BerandaFragment", "onAttach invoked")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("BerandaFragment", "onCreate invoked")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("BerandaFragment", "onViewCreated invoked")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("BerandaFragment", "onStart invoked")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("BerandaFragment", "onResume invoked")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("BerandaFragment", "onPause invoked")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("BerandaFragment", "onStop invoked")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("BerandaFragment", "onDestroyView invoked")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("BerandaFragment", "onDetach invoked")
+    }
+
 }
