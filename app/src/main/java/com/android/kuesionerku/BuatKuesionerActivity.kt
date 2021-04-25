52ed4ec33eb7b91f05377c48dd1b105c2800276a
victoriatrinita <victoriatrinita@yahoo.com>
2021-04-25 23:44:49 +0700

    feat: implement room database

---
+++ app/src/main/java/com/android/kuesionerku/BuatKuesionerActivity.kt
@@ -0,0 +1,11 @@
+package com.android.kuesionerku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuatKuesionerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_buat_kuesioner)
+    }
+}
